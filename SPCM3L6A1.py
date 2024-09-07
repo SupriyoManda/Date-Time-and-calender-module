e613676ffc755d7b10a93a711fc644c53b4fb130
Supriyo Mandal <supriyo.mandal27@gmail.com>
2024-09-07 11:16:57 +0530

    M19 python

---
+++ SPCM3L6A1.py
@@ -0,0 +1,7 @@
+from datetime import date,time,datetime
+
+today=date.today()
+now = datetime.now()
+print("Today date is",today)
+print("\ncurrent date an time is",now)
+print("Date component",today.year,today.month,today.day)
